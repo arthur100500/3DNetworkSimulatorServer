bc19c509dcf1217bf3b397d479e372ab629491fa
Arthur Alekseev <57834711+arthur100500@users.noreply.github.com>
2023-04-23 00:04:21 +0300

    Remove syncing stuff

--- src/3DNetworkSimulatorUI/my-app/src/components/UnityContainer/index.tsx
+++ src/3DNetworkSimulatorUI/my-app/src/components/UnityContainer/index.tsx
@@ -2,34 +2,15 @@ import React, { useCallback, useEffect, useState } from "react";
 import { Unity, useUnityContext, } from "react-unity-webgl";
 import gnsSettings from '../../configs/server.json';
 
-// Create Unity context
-const { unityProvider, sendMessage, addEventListener, removeEventListener } = useUnityContext({
-    loaderUrl: "/build/Build/build.loader.js",
-    dataUrl: "/build/Build/webgl.data",
-    frameworkUrl: "/build/Build/build.framework.js",
-    codeUrl: "/build/Build/build.wasm",
-});
-
-const [arg, requestGnsCofig] = useState();
-
-const handleRequestGnsCofig = useCallback(() => {
-    console.log("Requested configs, sending " + gnsSettings)
-    sendMessage(
-        "GlobalGnsParameters",
-        "SetGnsConfig",
-        (gnsSettings.addr, gnsSettings.port, gnsSettings.user)
-    );
-}, []);
-
-useEffect(() => {
-    addEventListener("RequestGnsCofig", requestGnsCofig);
-    return () => {
-        removeEventListener("RequestGnsCofig", requestGnsCofig);
-    };
-}, [addEventListener, removeEventListener, handleRequestGnsCofig]);
-
 const UnityContainer: React.FC = () => {
-    // Return page
+    // Create Unity context
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "/build/Build/build.loader.js",
+        dataUrl: "/build/Build/webgl.data",
+        frameworkUrl: "/build/Build/build.framework.js",
+        codeUrl: "/build/Build/build.wasm",
+    });
+
     return < Unity unityProvider={unityProvider} />;
 }
 
